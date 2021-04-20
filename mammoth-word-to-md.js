8d1c894e41b9d6a4ab7619b43fdda47823214410
Steven <steven.woods@digital.education.gov.uk>
2021-04-20 10:58:02 +0100

    tidy up js function to reduce callback hell

--- mammoth-word-to-md.js
+++ mammoth-word-to-md.js
@@ -1,79 +1,79 @@
-import mammoth from "mammoth"
-import fs from 'fs';
-import unified from 'unified';
-import parseHTML from 'rehype-parse';
-import rehype2remark from 'rehype-remark';
-import stringify from 'remark-stringify';
-import prettier from 'prettier';
+import mammoth from "mammoth";
+import fs from "fs";
+import fsPromises from "fs/promises";
+import unified from "unified";
+import parseHTML from "rehype-parse";
+import rehype2remark from "rehype-remark";
+import stringify from "remark-stringify";
+import prettier from "prettier";
 
+let coreInductionProgrammes = ["Ambition"];
 
-let coreInductionProgrammes = ["Ambition"]
-
-
- async function convertToMarkdown() {
-    coreInductionProgrammes.forEach((programme) => {
-        fs.readdir((`./mentormats/${programme}`), (err, folder) => {
-            fs.mkdir((`./mentormats/mammoth-saved-md/${programme}`), () => {
-                folder = folder.filter(f => !f.startsWith("."))
-                folder.forEach((block) => {
-                    fs.readdir((`./mentormats/${programme}/${block}`), (err, files) => {
-                        files = files.filter(f => !f.startsWith("."))
-                        files.map((file) => {
-                                mammoth.convertToHtml({path: `./mentormats/${programme}/${block}/${file}`}).then(async(result) => {
-                                    console.log("before processing html")
-                                    const markdown = await parseMarkdown(result)
-                                    console.log("after processing html")
-                                    if (fs.existsSync(`./mentormats/mammoth-saved-md/${programme}/${block}`)) {
-
-                                        fs.writeFile(`./mentormats/mammoth-saved-md/${programme}/${block}/${file.replace(".docx", ".mdx")}`, markdown, (err) => {
-                                            if (err) throw err
-                                            else console.log("it was saved")
-                                        })
-                                    } else fs.mkdir((`./mentormats/mammoth-saved-md/${programme}/${block}`), () => {
-                                        fs.writeFile(`./mentormats/mammoth-saved-md/${programme}/${block}/${file.replace(".docx", ".mdx")}`, markdown, (err) => {
-                                            if (err) throw err
-                                            else console.log("it was saved")
-                                        })
-                                    })
-                                }).done()
-                        })
-                    })
-                })
-            })
-        })
-    })
+async function convertToMarkdown() {
+  coreInductionProgrammes.forEach((programme) => {
+    fsPromises
+      .mkdir(`./mentormats/mammoth-saved-md/${programme}`)
+      .then(async () => {
+        const folders = await fsPromises.readdir(`./mentormats/${programme}`);
+        for await (const folder of folders)
+          fsPromises
+            .mkdir(`./mentormats/mammoth-saved-md/${programme}/${folder}`)
+            .then(async () => {
+              const files = await fsPromises.readdir(
+                `./mentormats/${programme}/${folder}`
+              );
+              for await (const file of files)
+                mammoth
+                  .convertToHtml({
+                    path: `./mentormats/${programme}/${folder}/${file}`,
+                  })
+                  .then(async (result) => {
+                    console.log("before processing html");
+                    const markdown = await parseMarkdown(result);
+                    console.log("after processing html");
+                    fsPromises.writeFile(
+                      `./mentormats/mammoth-saved-md/${programme}/${folder}/${file.replace(
+                        ".docx",
+                        ".mdx"
+                      )}`,
+                      markdown
+                    );
+                  })
+                  .then(() => {
+                    console.log("it was saved");
+                  });
+            });
+      });
+  });
 }
 
 function parseMarkdown(data) {
-    return new Promise((resolve, reject) => {
-        unified()
-            .use(parseHTML, {
-                fragment: true,
-                emitParseErrors: true,
-                duplicateAttribute: false,
-            })
-            .use(rehype2remark)
-            .use(stringify, {
-                fences: true,
-                listItemIndent: 1,
-                gfm: false,
-                pedantic: false,
-            })
-            .process(data, (err, dirtyMarkdown) => {
-                console.log("in the process")
-                if (err) {
-                    reject(err);
-                } else {
-                    // actual mdx string
-                    let content = dirtyMarkdown.value;
-                    content = content.replace(
-                        /(?<=https?:\/\/.*)\\_(?=.*\n)/g,
-                        '_',
-                    );
-                    resolve(prettier.format(content, {parser: 'mdx'}));
-                }
-            });
-    });
+  return new Promise((resolve, reject) => {
+    unified()
+      .use(parseHTML, {
+        fragment: true,
+        emitParseErrors: true,
+        duplicateAttribute: false,
+      })
+      .use(rehype2remark)
+      .use(stringify, {
+        fences: true,
+        listItemIndent: 1,
+        gfm: false,
+        pedantic: false,
+      })
+      .process(data, (err, dirtyMarkdown) => {
+        console.log("in the process");
+        if (err) {
+          reject(err);
+        } else {
+          // actual mdx string
+          let content = dirtyMarkdown.value;
+          content = content.replace(/(?<=https?:\/\/.*)\\_(?=.*\n)/g, "_");
+          resolve(prettier.format(content, { parser: "mdx" }));
+        }
+      });
+  });
 }
 
-convertToMarkdown()
+convertToMarkdown();
